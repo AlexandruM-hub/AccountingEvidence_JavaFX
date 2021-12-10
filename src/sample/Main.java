a234a6b649a917d4d6c4e8a603624a09f3bfda2a
Mazureac Alexandru <alexandru.mazureac@iis.utm.md>
2021-12-10 13:48:34 +0200

    Added encrypting to login stage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,23 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxmlAndControllers/App.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("fxmlAndControllers/login.fxml"));
         primaryStage.setScene(new Scene(root, 989, 583));
         Image iconIMG = new Image("file:D:\\Lucrari de laborator\\Semestrul 3\\Teza de an\\TezaProiect\\src\\css\\tractor.png");
         primaryStage.getIcons().add(iconIMG);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
--- src/sample/fxmlAndControllers/Controller.java
+++ src/sample/fxmlAndControllers/Controller.java
@@ -1,16 +1,23 @@
 package sample.fxmlAndControllers;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import sample.DatabaseConnection;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Controller {
 
@@ -23,44 +30,65 @@ public class Controller {
     @FXML
     private PasswordField passwordField;
 
-    public void cancelButtonOnAction(ActionEvent e){
+    public void cancelButtonOnAction(){
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
 
-    public void  loginButtonOnAction(ActionEvent e){
-        loginMessageError.setText("Username");
+    public void  loginButtonOnAction() throws NoSuchAlgorithmException {
         if(!usernameField.getText().isBlank() && !passwordField.getText().isBlank()){
             validateLogin();
         }
         else{
-            loginMessageError.setText("Login and password can't be null");
+            loginMessageError.setText("Completati campurile");
         }
-
     }
 
-    public void validateLogin(){
+    public void validateLogin() throws NoSuchAlgorithmException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(passwordField.getText().getBytes());
+        byte[] bytes = m.digest();
+        StringBuilder s = new StringBuilder();
+        for (byte aByte : bytes) {
+            s.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+        }
+        String encryptedString = s.toString();
         DatabaseConnection connectNow = new DatabaseConnection();
-        Connection connectDB = connectNow.getConnection();
-        String verifyLogin = "SELECT count(1) FROM login WHERE username ='" + usernameField.getText() + "'AND password ='" + passwordField.getText() +"'";
-
+        Connection conn = connectNow.getConnection();
+        String getPass = "select password from login where username = '" +usernameField.getText()+"'";
+        String verifyUser = "select count(1) from login where username = '" + usernameField.getText() +"'";
         try {
-            Statement statement = connectDB.createStatement();
-            ResultSet queryResult = statement.executeQuery(verifyLogin);
-
-            while(queryResult.next()){
-                if(queryResult.getInt(1)==1){
-                    loginMessageError.setText("Welcome"); //load new scene
+            ResultSet verifyUsername = conn.createStatement().executeQuery(verifyUser);
+            if(verifyUsername.next()){
+                if(verifyUsername.getInt(1)==1){
+                    ResultSet verifyPasswordResultSet = conn.createStatement().executeQuery(getPass);
+                    if(verifyPasswordResultSet.next()){
+                        if(verifyPasswordResultSet.getString("password").equals(encryptedString)){
+                            cancelButtonOnAction();
+                            loadAppScene();
+                        }else{
+                            loginMessageError.setText("Acces respins");
+                        }
+                    }
                 } else{
-                    loginMessageError.setText("Invalid login. Try again");
+                    loginMessageError.setText("Acces Respins");
                 }
             }
         }
-        catch(SQLException e){
+        catch(SQLException | IOException e){
             e.printStackTrace();
         }
 
     }
 
+    private void loadAppScene() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setScene(new Scene(root));
+        stage.setTitle("GT Mazureac Alexandru");
+        Image iconIMG = new Image("file:D:\\Lucrari de laborator\\Semestrul 3\\Teza de an\\TezaProiect\\src\\css\\tractor.png");
+        stage.getIcons().add(iconIMG);
+        stage.show();
+    }
 }
